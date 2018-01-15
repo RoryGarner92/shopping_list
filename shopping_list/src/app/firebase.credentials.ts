f1ba671ab6593194fdc21beb3e9652e11a00946c
Rory <rorygarner92@gmail.com>
2018-01-15 14:54:20 +0000

    connecting to firebase

---
+++ shopping_list/src/app/firebase.credentials.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+            apiKey: "AIzaSyCmPGaFyJASdyM6sKW1Nt17PEotpG16gOo",
+        authDomain: "shoppinglist-49ea9.firebaseapp.com",
+        databaseURL: "https://shoppinglist-49ea9.firebaseio.com",
+        projectId: "shoppinglist-49ea9",
+        storageBucket: "",
+        messagingSenderId: "529912401338"
+     
+}
